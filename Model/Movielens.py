bd219edb886f0e18c1a74f065e8a260cea05bf24
Aman Pandey <81262080+Amanp16@users.noreply.github.com>
2023-03-20 02:56:22 +0530

    updated model and evaluation methods

--- Model/Movielens.py
+++ Model/Movielens.py
@@ -116,9 +116,9 @@ class MovieLens:
                     years[movieID] = int(year)
         return years
     
-    def getMiseEnScene(self):
+    def getMiseEnScene(self): #new ways of recommendation inc;uding more features
         mes = defaultdict(list)
-        with open("LLVisualFeatures13K_Log.csv", newline='') as csvfile:
+        with open("'../dataset/LLVisualFeatures13K_Log.csv'", newline='') as csvfile:
             mesReader = csv.reader(csvfile)
             next(mesReader)
             for row in mesReader:
